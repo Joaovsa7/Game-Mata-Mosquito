bdb61d6ca4dce58075041644e59be888bba4d901
joaovsa7 <jvsadesign@gmail.com>
2018-09-24 21:10:19 -0300

    mosquito já está se posicionando aleatoriamente

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ function windowAdjust(){
 
 windowAdjust()
 
-let posicaoX = Math.floor(Math.random() * largura);
-let posicaoY = Math.floor(Math.random() * altura);
+let posicaoX = Math.floor(Math.random() * largura) -90;
+let posicaoY = Math.floor(Math.random() * altura) -90;
 
 posicaoX = posicaoX < 0 ? 0 : posicaoX
 posicaoY = posicaoY < 0 ? 0 : posicaoY
@@ -20,15 +20,37 @@ console.log(posicaoX, posicaoY)
 
 let mosca = document.createElement('img');
 mosca.src = 'imagens/mosca.png'
-mosca.className = 'mosquito1'
+mosca.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
 mosca.style.left = posicaoX + 'px'
 mosca.style.top = posicaoY + 'px'
 mosca.style.position = 'absolute'
 document.body.appendChild(mosca)
-tamanhoAleatorio();
 
 
 function tamanhoAleatorio(){
-    var classe = Math.floor(Math.random() * 3)
+    let classe = Math.floor(Math.random() * 3)
     console.log(classe)
+    switch(classe){
+        case 0:
+        return 'mosquito1'
+        
+        case 1:
+        return 'mosquito2'
+        
+        case 2:
+        return 'mosquito3'
+    }
+}
+
+function ladoAleatorio(){
+    let classe = Math.floor(Math.random() * 2)
+    console.log(classe)
+    switch(classe){
+        case 0:
+        return 'ladoA'
+        
+        case 1:
+        return 'ladoB'
+  
+    }
 }
