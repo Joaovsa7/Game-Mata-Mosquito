0d10cefd35cb31d8f093c9887a9d663ca448d315
joaovsa7 <jvsadesign@gmail.com>
2018-09-25 09:09:20 -0300

    sistema de vitoria e derrota adicionado, inserção de level

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 
 let largura = 0
 let altura = 0 
+let vidas = 1
+let tempo = 25
+let modal = document.querySelector('#meuModal');
+let modalV = document.querySelector('#modalV')
+let btnVitoria = document.querySelector('#btnVitoria')
+let btnDerrota = document.querySelector('#btnDerrota')
+
 
 function windowAdjust(){
-
+    
     altura = window.innerHeight
     largura = window.innerWidth
     console.log(altura, largura)
@@ -13,28 +20,41 @@ function windowAdjust(){
 function logicaGame(){    
     //remover o mosquito anterior (se existir)   
     if(document.querySelector('#mosca')) { 
-document.querySelector('#mosca').remove()
+        document.querySelector('#mosca').remove()
+        if(vidas > 3) { 
+            modal.style.display = "flex"
+        } else {
+            
+            document.querySelector('#v' + vidas).src = "imagens/coracao_vazio.png"
+            vidas++
+        }
+        
+        
+    }
+    let posicaoX = Math.floor(Math.random() * largura) -90;
+    let posicaoY = Math.floor(Math.random() * altura) -90;
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+    console.log(posicaoX, posicaoY)
+    let mosca = document.createElement('img');
+    mosca.src = 'imagens/mosca.png'
+    mosca.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
+    mosca.style.left = posicaoX + 'px'
+    mosca.style.top = posicaoY + 'px'
+    mosca.style.position = 'absolute'
+    mosca.id = 'mosca'
+    mosca.onclick = function(){
+        
+        this.remove()
+    }
+    document.body.appendChild(mosca)
+    
 }
-let posicaoX = Math.floor(Math.random() * largura) -90;
-let posicaoY = Math.floor(Math.random() * altura) -90;
-posicaoX = posicaoX < 0 ? 0 : posicaoX
-posicaoY = posicaoY < 0 ? 0 : posicaoY
-console.log(posicaoX, posicaoY)
-let mosca = document.createElement('img');
-mosca.src = 'imagens/mosca.png'
-mosca.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
-mosca.style.left = posicaoX + 'px'
-mosca.style.top = posicaoY + 'px'
-mosca.style.position = 'absolute'
-mosca.id = 'mosca'
-document.body.appendChild(mosca)
 
- }
-
- windowAdjust()
-setInterval(function() {
+windowAdjust()
+let criaMosca = setInterval(function() {
     logicaGame()
- }, 1000)
+}, 1000)
 function tamanhoAleatorio(){
     let classe = Math.floor(Math.random() * 3)
     console.log(classe)
@@ -59,6 +79,18 @@ function ladoAleatorio(){
         
         case 1:
         return 'ladoB'
-  
+        
     }
 }
+
+let cronometro = setInterval(function(){
+    tempo -= 1
+    if(tempo < 0 ){
+        clearInterval(cronometro)
+        clearInterval(criaMosca)  
+     modalV.style.display = "flex"
+     btnVitoria.onclick = function(){
+        location.reload()
+     }
+    }else {document.querySelector('#cronometro').innerHTML = tempo
+}},1000)
